5ff92a77121c242cc9498a8abb21d4235ac5db74
cleanfinedust <119744230+cleanfinedust@users.noreply.github.com>
2023-05-06 00:38:54 +0900

    Create hack.php

---
+++ hack.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>System Command Execution</title>
+</head>
+<body>
+    <h1>Execute System Command</h1>
+    <form action="execute.php" method="POST">
+        <label for="command">Command:</label>
+        <input type="text" name="command" id="command" required>
+        <br><br>
+        <input type="submit" value="Execute">
+    </form>
+</body>
+</html>
